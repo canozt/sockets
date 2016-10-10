a39a7def7fd9842c9fcb6614225078f14ffc67ff
canoz <canozturk0@gmail.com>
2016-10-10 10:49:25 -0400

    timestamp

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
+var moment = require('moment');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -13,11 +14,13 @@ io.on('connection', function(socket){
     socket.on('message', function(message){
         console.log('Message received: ' + message.text);
             
+        message.timeStamp = moment().valueOf();
         io.emit('message', message);
     });
     
     socket.emit('message', {
-        text: 'Welcome to the chat app!'
+        text: 'Welcome to the chat app!',
+        timestamp: moment().valueOf()
     });
 });
 
